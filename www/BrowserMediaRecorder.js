6c955dab00f613f32e0a873367da447489348cf0
joey <joeyw@reallyenglish.com>
2015-01-26 16:24:58 +0000

    fix js file for the plugin

--- www/BrowserMediaRecorder.js
+++ www/BrowserMediaRecorder.js
@@ -13,39 +13,39 @@ var mediaObjects = {};
  * @param errorCallback         The callback to be called if there is an error.
  *                                  errorCallback(int errorCode) - OPTIONAL
  */
-var Media = function(successCallback, errorCallback) {
-    argscheck.checkArgs('FF', 'Media', arguments);
+var MediaRecorder = function(successCallback, errorCallback) {
+    argscheck.checkArgs('FF', 'MediaRecorder', arguments);
     this.id = utils.createUUID();
     mediaObjects[this.id] = this;
     this.successCallback = successCallback;
     this.errorCallback = errorCallback;
-    exec(null, this.errorCallback, "Media", "create", [this.id, this.src]);
+    exec(null, this.errorCallback, "MediaRecorder", "create", [this.id, this.src]);
 };
 
 // "static" function to return existing objs.
-Media.get = function(id) {
+MediaRecorder.get = function(id) {
     return mediaObjects[id];
 };
 
 /**
  * Start recording.
  */
-Media.prototype.startRecord = function() {
-    exec(null, this.errorCallback, "Media", "startRecordingAudio", [this.id, this.src]);
+MediaRecorder.prototype.startRecord = function() {
+    exec(null, this.errorCallback, "MediaRecorder", "startRecordingAudio", [this.id, this.src]);
 };
 
 /**
  * Stop recording.
  */
-Media.prototype.stopRecord = function() {
-    exec(null, this.errorCallback, "Media", "stopRecordingAudio", [this.id]);
+MediaRecorder.prototype.stopRecord = function() {
+    exec(null, this.errorCallback, "MediaRecorder", "stopRecordingAudio", [this.id]);
 };
 
 /**
  * Release the resources.
  */
-Media.prototype.release = function() {
-    exec(null, this.errorCallback, "Media", "release", [this.id]);
+MediaRecorder.prototype.release = function() {
+    exec(null, this.errorCallback, "MediaRecorder", "release", [this.id]);
 };
 
-module.exports = Media;
+module.exports = MediaRecorder;
