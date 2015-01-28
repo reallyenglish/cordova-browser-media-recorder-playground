9c0a76362c6c40b4a050c2f122c288946f8b388a
joey <joeyw@reallyenglish.com>
2015-01-28 00:24:39 +0000

    single recorder for all the request

--- src/browser/MediaRecorderProxy.js
+++ src/browser/MediaRecorderProxy.js
@@ -9,38 +9,28 @@ var Recorder = {
 
     // Initiates the audio file
     create:function(win, lose, args) {
-        var id = args[0];
-        var thisM = BrowserMediaRecorder.get(id);
-        thisM.recorder = MRecorder.getInstance({swfSrc: 'scripts/recorder.swf'});
+        this.mRecorder = MRecorder.getInstance({swfSrc: 'scripts/recorder.swf'});
     },
 
     // Start recording audio
     startRecordingAudio:function(win, lose, args) {
-        var id = args[0];
-        var thisM = BrowserMediaRecorder.get(id);
-        thisM.recorder.record();
+        this.mRecorder.record();
     },
 
     // Stop recording audio
     stopRecordingAudio:function(win, lose, args) {
-        var id = args[0];
-        var thisM = BrowserMediaRecorder.get(id);
-        thisM.recorder.stop();
+        this.mRecorder.stop();
     },
 
     // play back recording audio
     startPlayingAudio:function(win, lose, args) {
-        var id = args[0];
-        var thisM = BrowserMediaRecorder.get(id);
-        thisM.recorder.play();
+        this.mRecorder.play();
     },
 
     // Release the media object
     release:function(win, lose, args) {
-        var id = args[0];
-        var thisM = BrowserMediaRecorder.get(id);
         try {
-            delete thisM.recorder;
+            delete this.mRecorder;
         } catch (err) {
             lose("Failed to release: "+err);
         }
